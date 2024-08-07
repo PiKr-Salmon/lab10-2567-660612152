31481f3ad396db3aad05c2c1e63c7d72a2ec2785
PiKr-Salmon <piyawat_kr@cmu.ac.th>
2024-08-07 17:28:11 +0700

    Complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,37 @@
 "use client";
 
 import axios from "axios";
-import { useState } from "react";
+import React, { use, useState } from "react";
+import { cleanUser } from "@/libs/cleanUser";
+import { useEffect } from "react";
+import UserCard from "@/components/UserCard";
+import { UserCardProps } from "@/libs/types";
 
 export default function RandomUserPage() {
   // annotate type for users state variable
-  const [users, setUsers] = useState(null);
+  const [users, setUsers] = useState<any>(null);
 
   const [isLoading, setIsLoading] = useState(false);
-  const [genAmount, setGenAmount] = useState(1);
+  const [genAmount, setGenAmount] = useState("");
+  const [isFirstLoad, setIsFirstLoad] = useState(true);
 
+  useEffect(() => {
+    if(isFirstLoad){
+      setIsFirstLoad(false);
+      return;
+    }
+    const jsonstr = JSON.stringify(genAmount);
+    localStorage.setItem('users',jsonstr);
+  },[genAmount]);
+
+  useEffect(() => {
+    const jsonstr = localStorage.getItem("users");
+    if(jsonstr !== null){
+      const newAmount = JSON.parse(jsonstr);
+      setGenAmount(newAmount);
+    }
+  },[]);
+  
   const generateBtnOnClick = async () => {
     setIsLoading(true);
     const resp = await axios.get(
@@ -17,10 +39,11 @@ export default function RandomUserPage() {
     );
     setIsLoading(false);
     const users = resp.data.results;
-
     //Your code here
     //Process result from api response with map function. Tips use function from /src/libs/cleanUser
     //Then update state with function : setUsers(...)
+    const cleanedUser = users.map((users : UserCardProps) => cleanUser(users));
+    setUsers(cleanedUser);
   };
 
   return (
@@ -42,7 +65,7 @@ export default function RandomUserPage() {
       {isLoading && (
         <p className="display-6 text-center fst-italic my-4">Loading ...</p>
       )}
-      {users && !isLoading && users.map(/*code map rendering UserCard here */)}
+      {users && !isLoading && users.map((users : UserCardProps) => { return <UserCard {...users}/>})}
     </div>
   );
 }
--- src/libs/types.ts
+++ src/libs/types.ts
@@ -1,3 +1,13 @@
-// add type for UserCard's Props
-
-// add type for UserCardDetail's Props
+interface UserCardDetailProps {
+    email: string;
+    address: string;
+  }
+  export type { UserCardDetailProps };
+  
+  interface UserCardProps {
+    name: string;
+    email: string;
+    imgUrl: string;
+    address : string;
+  }
+  export type { UserCardProps };
